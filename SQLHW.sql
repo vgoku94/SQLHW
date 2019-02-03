ee61b0a0f95588e33b239934aa4e549c97812dd1
vgoku94 <vgoku94@gmail.com>
2019-02-03 17:13:57 -0500

    SQL HW
    Update

--- SQLHW.sql
+++ SQLHW.sql
@@ -1,113 +1,113 @@
--- 1a. 
+#1a
 USE sakila;
 SELECT first_name, last_name 
 FROM actor;
--- 1b. 
+#1b
 SELECT CONCAT(first_name,  ' ', last_name) AS ' Actor Name'
 FROM actor;
--- 2a.
+#2a
 SELECT actor_id, first_name, last_name 
 FROM actor
 WHERE first_name = 'JOE';
--- 2b.
+#2b
 SELECT actor_id, first_name, last_name 
 FROM actor
 WHERE last_name LIKE '%GEN%';
--- 2c. 
+#2c
 SELECT actor_id, first_name, last_name 
 FROM actor
 WHERE last_name LIKE '%LI%'
 ORDER BY last_name, first_name;
--- 2d. 
+#2d
 SELECT country_id,  country
 FROM country
 WHERE country IN ('Afghanistan', 'Bangladesh', 'China');
--- 3a.
+#3a
 SELECT * FROM actor;
 ALTER TABLE actor
 ADD COLUMN  middle_name VARCHAR(50) AFTER first_name;
--- 3b.
+#3b
 ALTER TABLE actor 
 MODIFY COLUMN middle_name BLOB;
--- 3c. 
+#3c 
 ALTER TABLE actor
 DROP COLUMN middle_name;
--- 4a. 
+#4a
 SELECT last_name, COUNT(*) AS `Count`
 FROM actor
 GROUP BY last_name;
--- 4b. 
+#4b 
 SELECT last_name, COUNT(*) AS `Count`
 FROM actor
 GROUP BY last_name
 HAVING Count > 2;
--- 4c.
+#4c
 UPDATE actor 
 SET first_name= 'HARPO'
 WHERE first_name='GROUCHO' AND last_name='WILLIAMS';
--- 4d.
+#4d
 UPDATE actor 
 SET first_name= 'GROUCHO'
 WHERE first_name='HARPO' AND last_name='WILLIAMS';
--- 5a. 
+#5a
 DESCRIBE sakila.address;
--- 6a. 
+#6a
 SELECT s.first_name, s.last_name, a.address
 FROM staff s LEFT JOIN address a ON s.address_id = a.address_id;
--- 6b. 
+#6b
 SELECT s.first_name, s.last_name, SUM(p.amount) AS 'TOTAL'
 FROM staff s LEFT JOIN payment p  ON s.staff_id = p.staff_id
 GROUP BY s.first_name, s.last_name;
--- 6c. 
+#6c
 SELECT f.title, COUNT(a.actor_id) AS 'TOTAL'
 FROM film f LEFT JOIN film_actor  a ON f.film_id = a.film_id
 GROUP BY f.title;
--- 6d. 2
--- 6e.
+#6d 2
+#6e
 SELECT c.first_name, c.last_name, SUM(p.amount) AS 'TOTAL'
 FROM customer c LEFT JOIN payment p ON c.customer_id = p.customer_id
 GROUP BY c.first_name, c.last_name
 ORDER BY c.last_name;
--- 7a. 
+#7a
 USE sakila;
 SELECT title
 FROM film
 WHERE (title LIKE 'K%' OR title LIKE 'Q%') 
 AND language_id=(SELECT language_id FROM language where name='English');
--- 7b.
+#7b
 SELECT first_name, last_name
 FROM actor
 WHERE actor_id
-	IN (SELECT actor_id FROM film_actor WHERE film_id 
-		IN (SELECT film_id from film where title='ALONE TRIP'));
--- 7c. 
+IN (SELECT actor_id FROM film_actor WHERE film_id 
+IN (SELECT film_id from film where title='ALONE TRIP'));
+#7c
 SELECT first_name, last_name, email 
 FROM customer cu
 JOIN address a ON (cu.address_id = a.address_id)
 JOIN city cit ON (a.city_id=cit.city_id)
 JOIN country cntry ON (cit.country_id=cntry.country_id)
 WHERE cntry.country = "CANADA";
--- 7d. 
+#7d
 SELECT title from film f
 JOIN film_category fcat on (f.film_id=fcat.film_id)
 JOIN category c on (fcat.category_id=c.category_id);
--- 7e.
+#7e
 SELECT title, COUNT(f.film_id) AS 'Count_of_Rented_Movies'
 FROM  film f
 JOIN inventory i ON (f.film_id= i.film_id)
 JOIN rental r ON (i.inventory_id=r.inventory_id)
 GROUP BY title ORDER BY Count_of_Rented_Movies DESC;
--- 7f. 
+#7f
 SELECT s.store_id, SUM(p.amount) 
 FROM payment p
 JOIN staff s ON (p.staff_id=s.staff_id)
 GROUP BY store_id;
--- 7g.
+#7g
 SELECT store_id, city, country FROM store s
 JOIN address a ON (s.address_id=a.address_id)
 JOIN city c ON (a.city_id=c.city_id)
 JOIN country cntry ON (c.country_id=cntry.country_id);
--- 7h. 
+#7h
 SELECT c.name AS "Top Five", SUM(p.amount) AS "Gross" 
 FROM category c
 JOIN film_category fc ON (c.category_id=fc.category_id)
@@ -115,7 +115,7 @@ JOIN inventory i ON (fc.film_id=i.film_id)
 JOIN rental r ON (i.inventory_id=r.inventory_id)
 JOIN payment p ON (r.rental_id=p.rental_id)
 GROUP BY c.name ORDER BY Gross  LIMIT 5;
--- 8a.
+#8a
 SELECT c.name AS "Top Five", SUM(p.amount) AS "Gross" 
 FROM category c
 JOIN film_category fc ON (c.category_id=fc.category_id)
@@ -123,7 +123,7 @@ JOIN inventory i ON (fc.film_id=i.film_id)
 JOIN rental r ON (i.inventory_id=r.inventory_id)
 JOIN payment p ON (r.rental_id=p.rental_id)
 GROUP BY c.name ORDER BY Gross  LIMIT 5;
--- 8b.
+#8b
 SELECT* FROM TopFive;
--- 8c.
+#8c
 DROP VIEW TopFive
